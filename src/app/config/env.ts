579d518788179203b48586702379cbdaf84bbf11
Md Jahidul Islam <nabinjahid@gmail.com>
2025-07-16 06:13:44 +0600

    userTokens functions created

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -8,13 +8,15 @@ interface EnvConfig {
     NODE_ENV:"development" | "production";
     JWT_ACCESS_SECRET:string;
     JWT_ACCESS_EXPIRE:string;
+    JWT_REFRESH_SECRET:string;
+    JWT_REFRESH_EXPIRE:string;
     BCRYPT_SALTROUND:string;
     SUPER_ADMIN_EMAIL:string;
     SUPER_ADMIN_PASSWORD:string
 }
 
 const loadEnvVariables = (): EnvConfig =>{
-    const requiredEnvVariables : string[] = ["PORT", "DB_URL", "NODE_ENV", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRE", "BCRYPT_SALTROUND", "SUPER_ADMIN_PASSWORD", "SUPER_ADMIN_EMAIL"]
+    const requiredEnvVariables : string[] = ["PORT", "DB_URL", "NODE_ENV", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRE", "BCRYPT_SALTROUND", "SUPER_ADMIN_PASSWORD", "SUPER_ADMIN_EMAIL", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRE"]
 
     requiredEnvVariables.forEach(key =>{
         if (!process.env[key]) {
@@ -31,7 +33,10 @@ const loadEnvVariables = (): EnvConfig =>{
         JWT_ACCESS_EXPIRE: process.env.JWT_ACCESS_EXPIRE as string,
         BCRYPT_SALTROUND: process.env.BCRYPT_SALTROUND as string,
         SUPER_ADMIN_EMAIL:process.env.SUPER_ADMIN_EMAIL as string,
-        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+        JWT_REFRESH_SECRET:process.env.JWT_REFRESH_SECRET as string,
+        JWT_REFRESH_EXPIRE:process.env.JWT_REFRESH_EXPIRE as string
+
     }
 }
 
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,42 +1,49 @@
 import AppError from "../../errorHelpers/AppError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
-import httpStatus from "http-status-codes"
-import bcryptjs from "bcryptjs"
-import { generateToken } from "../../utils/jwt";
-import { envVars } from "../../config/env";
+import httpStatus from "http-status-codes";
+import bcryptjs from "bcryptjs";
+import { createUserToken } from "../../utils/userToken";
 
-const credentialsLogin = async(payload: Partial<IUser>)=>{
-    const {email , password} = payload;
+const credentialsLogin = async (payload: Partial<IUser>) => {
+  const { email, password } = payload;
 
-    const isUserExist = await User.findOne({email})
+  const isUserExist = await User.findOne({ email });
 
-    if (!isUserExist) {
-        throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist")
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
+  }
 
-    }
+  const isPasswordMatched = await bcryptjs.compare(
+    password as string,
+    isUserExist.password as string
+  );
 
-    const isPasswordMatched = await bcryptjs.compare(password as string, isUserExist.password as string)
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect password");
+  }
 
-    if (!isPasswordMatched) {
-        throw new AppError (httpStatus.BAD_REQUEST, "Incorrect password")
-    }
+  // Json web Token
+  // const jwtPayload = {
+  //     userId : isUserExist._id,
+  //     email : isUserExist.email,
+  //     role : isUserExist.role
+  // }
+  // const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRE)
+//   const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRE)
 
+  const userTokens = createUserToken(isUserExist);
 
-    // Json web Token
-    const jwtPayload = {
-        userId : isUserExist._id,
-        email : isUserExist.email,
-        role : isUserExist.role
-    }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: pass, ...rest } = isUserExist;
 
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRE)
-
-    return {
-        accessToken
-    }
-}
+  return {
+    accessToken: userTokens.accessToken,
+    refreshToken: userTokens.refreshToken,
+    user: rest,
+  };
+};
 
 export const AuthServices = {
-    credentialsLogin
-}
+  credentialsLogin,
+};
